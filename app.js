8d796dada8e5ca762cdf12688086b938c148d82b
ctiller15 <christiller2015@gmail.com>
2017-08-01 16:34:36 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var sessionLength = 25;
+var breakLength = 5;
+
+var sessSect = document.querySelector(".session");
+var breakSect = document.querySelector(".break");
+
+sessSect.textContent = sessionLength;
+breakSect.textContent = breakLength;
+
+console.log(sessionLength);
+console.log(breakLength);
+
