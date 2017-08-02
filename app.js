a67daa6a694f402b8edc5cdb3d5ee40b12dcc370
ctiller15 <christiller2015@gmail.com>
2017-08-01 21:38:32 -0400

    Add value modification w/ buttons

--- app.js
+++ app.js
@@ -1,12 +1,53 @@
-var sessionLength = 25;
-var breakLength = 5;
+var lengthObj = {
+	sessionLength: 25,
+	breakLength: 5
+};
 
 var sessSect = document.querySelector(".session");
 var breakSect = document.querySelector(".break");
 
-sessSect.textContent = sessionLength;
-breakSect.textContent = breakLength;
+var decreaseButtons = document.querySelectorAll(".dec");
+var increaseButtons = document.querySelectorAll(".inc");
 
-console.log(sessionLength);
-console.log(breakLength);
+// create a function that when called, increases the value.
+// It should update a desired element.
 
+var increment = (obj, key, varName) => {
+	if (obj[key] < 50) {
+		obj[key] += 1;
+	}
+	varName.textContent = obj[key];
+}
+
+// create a function that when called, decreases a value.
+
+var decrement = (obj, key, varName) => {
+	if (obj[key] > 1) {
+		obj[key] -= 1;
+	}
+	varName.textContent = obj[key];	
+}
+
+var primeButtons = (index, key, section ) => {
+	decreaseButtons[index].addEventListener("click", () => {
+		decrement(lengthObj, key, section);
+	});
+
+	increaseButtons[index].addEventListener("click", () => {
+		increment(lengthObj, key, section);
+	});
+}
+
+// Both values must always be 1 or greater.
+
+// initializing the display.
+sessSect.textContent = lengthObj.sessionLength;
+breakSect.textContent = lengthObj.breakLength;
+
+// index 0 refers to the "break" buttons, while
+// index 1 refers to the "session" buttons.
+// Couldn't really think of a way to make this one nicer.
+
+primeButtons(0, "breakLength", breakSect);
+
+primeButtons(1, "sessionLength", sessSect);
